b5716799d3ea48d9d4bffba2c4c56eee455fbad8
Ahmed Hany <klbala2007@gmail.com>
2016-03-31 13:04:40 +0200

    html & server.js files updated

--- server.js
+++ server.js
@@ -2,12 +2,45 @@
 
     // set up ========================
     var express  = require('express');
-    var app      = express();                               // create our app w/ express
+    var app      = express();
+    var bodyParser     = require('body-parser');
+    var methodOverride = require('method-override');                               // create our app w/ express
     
     // configuration =================
     app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 
     // routes ======================================================================
+    var html_dir = './public/html/';
+
+    
+    app.get('/boarding', function(req, res) {
+        
+        res.sendfile(html_dir + 'boarding.html');
+    });
+    
+    app.get('/confirm', function(req, res) {
+        
+        res.sendfile(html_dir + 'confirm.html');
+    });
+
+    app.get('/flight', function(req, res) {
+        
+        res.sendfile(html_dir + 'flight.html');
+    });
+
+    app.get('/trackflight', function(req, res) {
+        
+        res.sendfile(html_dir + 'trackFlight.html');
+    });
+
+    app.get('/viewflights', function(req, res) {
+        
+        res.sendfile(html_dir + 'viewFlights.html');
+    });
+
+
+
+    
 
 
     // application -------------------------------------------------------------
