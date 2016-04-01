562c00fa47d56bdfbbf6f6347c0a855e74f02ffa
tefa1 <Moustafa.Mahm@gmail.com>
2016-04-01 17:49:07 +0200

    Fixed a bug in flights route

--- routes.js
+++ routes.js
@@ -1,17 +1,17 @@
-// routes.js  	
+// routes.js
 
 module.exports = function(app) {
 
         // server routes ===========================================================
 
         app.get('/api/flights', function(req, res) {
-        	
-        	//res.send();    
+
+        //	res.send();
         });
 
-        
+
         app.get('*', function(req, res) {
-            res.sendfile('./public/views/index.html'); // load our public/index.html file
+            res.sendfile('./public/index.html'); // load our public/index.html file
         });
 
     };
