e270560b411b8b0752e2ab8857922fda2d398247
amr-mahmoud <amr.hazem.mahmoud@gmail.com>
2016-05-03 23:31:07 +0200

    Front aPP ionic

---
+++ [AppIonic]/www/js/controllers.js
@@ -0,0 +1,30 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cartTabDefaultPageCtrl', function($scope) {
+
+})
+   
+.controller('cloudTabDefaultPageCtrl', function($scope) {
+
+})
+         
+.controller('viewFlightsCtrl', function($scope) {
+
+})
+   
+.controller('contactUsCtrl', function($scope) {
+
+})
+   
+.controller('trackAFlightCtrl', function($scope) {
+
+})
+   
+.controller('pageCtrl', function($scope) {
+
+})
+ 
